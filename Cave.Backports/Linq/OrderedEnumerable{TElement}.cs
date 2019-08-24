5b31b8685bf67765f87309807ce57fb8c09af882
gernot <g.lenkner@outlook.de>
2019-08-24 17:35:52 +0200

    added additional linq backports

---
+++ Cave.Backports/Linq/OrderedEnumerable{TElement}.cs
@@ -0,0 +1,36 @@
+﻿#if NET20
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace System.Linq
+{
+    sealed class OrderedEnumerable<TElement> : IOrderedEnumerable<TElement>
+    {
+        readonly IEnumerable<TElement> elements;
+
+        public OrderedEnumerable(IEnumerable<TElement> elements) => this.elements = elements;
+
+        public IOrderedEnumerable<TElement> CreateOrderedEnumerable<TKey>(Func<TElement, TKey> keySelector, IComparer<TKey> comparer, bool descending)
+        {
+            var dict = new SortedDictionary<TKey, TElement>(comparer);
+            foreach (var element in elements)
+            {
+                dict.Add(keySelector(element), element);
+            }
+            var sorted = dict.Values.ToList();
+            if (descending)
+            {
+                sorted.Reverse();
+            }
+
+            return new OrderedEnumerable<TElement>(sorted);
+        }
+
+        public IEnumerator<TElement> GetEnumerator() => elements.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => elements.GetEnumerator();
+    }
+}
+
+#endif
